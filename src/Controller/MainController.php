2a1285bd750010e4f3fc45e0623fc4095bdbf8ff
dsanchez <dsanchez@campus-eni.fr>
2024-10-21 16:47:03 +0200

    Mise en place de twig et des controllers Module 02 et Module 03 jusqu'à demo 03

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        //return new Response("<h1>Accueil</h1>");
+        return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/test', name: 'main_test', methods: ['GET'])]
+    public function test(): Response
+    {
+        //return new Response("<h1>Test</h1>");
+        return $this->render('main/test.html.twig');
+    }
+}
