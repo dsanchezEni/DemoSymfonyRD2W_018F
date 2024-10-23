5f0e730955d3dd38925c8d1cc00df1bd7739fce4
dsanchez <dsanchez@campus-eni.fr>
2024-10-23 11:53:07 +0200

    Demo 01 et 02 Module 05 Mise en place de la BD et mise en place de l'entité Course avec comment inserer mettre à jour et supprimer

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/cours', name: 'app_cours_')]
@@ -36,9 +40,50 @@ class CourseController extends AbstractController
     }
 
     #[Route('/create', name: 'create',methods: ['GET','POST'])]
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        //dd($request);
+        dump($request);
         //TODO Rechercher dans la BD le cours en fonction de son ID pour le mettre à jour.
+        return $this->render('course/create.html.twig',
+            []
+        );
+    }
+
+    //J'injecte l'entity manager qui s'appelle $em grâce à l'injection de dépendance.
+    #[Route('/demo', name: 'demo',methods: ['GET'])]
+    public function demo(EntityManagerInterface $em): Response
+    {
+        //Créer un Objet de type Course et je l'instancie en faisant new
+        //Appel implicite au constructeur de l'objet Course.
+        $course = new Course();
+
+        //hydrater toutes les propriétés de l'objet Course.
+        $course->setName('Symfony');
+        $course->setContent('Le développement web côté Serveur (avec Symfony)');
+        $course->setDuration(10);
+        $course->setDateCreated(new \DateTimeImmutable());
+        $course->setPublished(true);
+
+        //Enregistrer dans la base de donnée
+        $em->persist($course);
+        dump($course);
+        //Penser à utiliser le flush sinon l'objet ne sera pas persisté dans la bd.
+        $em->flush($course);
+
+        dump($course);
+
+        //On modifie l'objet
+        $course->setName('PHP');
+        //!Pas besoin de faire le persist car Doctrine connait deja l'objet.
+        //Sauvegarde l'objet
+        $em->flush($course);
+        dump($course);
+
+        //Je supprime mon objet
+        $em->remove($course);
+        $em->flush();
+
         return $this->render('course/create.html.twig',
             []
         );
---
+++ src/Entity/Course.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CourseRepository::class)]
+#[ORM\Table(name: 'cours')]
+class Course
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(name:'libelle', length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $duration = null;
+
+    #[ORM\Column(options: ['default' => false])]
+    private ?bool $published = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateCreated = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateModified = null;
+
+    /**
+     * Constructeur
+     */
+    public function __construct()
+    {
+        //Permet d'initialiser l'attribut published à false quand je crée l'objet Course.
+        $this->published = false;
+    }
+
+    //-------------------------------------------------
+    //Getter et Setter
+    //-------------------------------------------------
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): static
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeImmutable
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeImmutable $dateCreated): static
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateModified(): ?\DateTimeImmutable
+    {
+        return $this->dateModified;
+    }
+
+    public function setDateModified(?\DateTimeImmutable $dateModified): static
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+}
